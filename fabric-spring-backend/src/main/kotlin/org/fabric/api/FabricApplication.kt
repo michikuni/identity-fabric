bf95891cb8c643ef4476a36ce5d43196ec015802
Minh Phuong Dang <minhphuonglcby@gmail.com>
2026-04-13 02:52:47 +0700

    Fix: expose auto-config classes error

--- fabric-spring-backend/src/main/kotlin/org/fabric/api/FabricApplication.kt
+++ fabric-spring-backend/src/main/kotlin/org/fabric/api/FabricApplication.kt
@@ -1,19 +1,9 @@
 package org.fabric.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    exclude = [
-        DataSourceAutoConfiguration::class,
-        HibernateJpaAutoConfiguration::class,
-        SecurityAutoConfiguration::class,
-    ],
-    scanBasePackages = ["org.fabric.api"]
-)
+@SpringBootApplication(scanBasePackages = ["org.fabric.api"])
 class FabricApplication
 
 fun main(args: Array<String>) {
